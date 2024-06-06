e84fa9b6ef7c1eda7110133159439949c95144e2
Luis Fernando <luis.almeida.faria@gmail.com>
2024-06-05 21:23:39 -0300

    Adicionando arquivos de configuração do docker e docker-compose

---
+++ src/test/java/com/luis/VibeTunes/VibeTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.VibeTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
