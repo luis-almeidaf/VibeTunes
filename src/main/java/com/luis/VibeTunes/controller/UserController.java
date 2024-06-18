a811b9da9fa16cf91a0257f9353af994c406b035
Luis Fernando <luis.almeida.faria@gmail.com>
2024-06-17 22:22:27 -0300

    fix: Correção nos endpoints do usuário

--- src/main/java/com/luis/VibeTunes/controller/UserController.java
+++ src/main/java/com/luis/VibeTunes/controller/UserController.java
@@ -5,32 +5,31 @@ import com.luis.VibeTunes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping()
     public ResponseEntity<List<User>> listUsers() {
         List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "/users/{username}")
+    @GetMapping(value = "/username/{username}")
     public ResponseEntity<User> findUserByUsername(@PathVariable String username) {
         var user = userService.findUserByUsername(username);
         return ResponseEntity.ok().body(user);
     }
 
-    @GetMapping(value = "/users/{id}")
-    public ResponseEntity<User> findUserById (@PathVariable Long id) throws Exception {
+    @GetMapping(value = "id/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id) throws Exception {
         var user = userService.findUserById(id);
         return ResponseEntity.ok(user);
     }
